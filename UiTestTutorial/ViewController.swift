40ed32120f89b5acc6d1d5f0aaf4299e4dc71f84
Alyona <albsch57@gmail.com>
2023-07-22 10:09:51 +0300

    Initial Commit

---
+++ UiTestTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiTestTutorial
+//
+//  Created by Alyona Bedrosova on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
